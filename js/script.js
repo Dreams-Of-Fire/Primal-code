fb32b1913947fb1b24e91d7a2ef1c8618d15c529
Dresms-Of-Fire <barmen.tivo@mail.ru>
2023-03-01 21:04:21 +0500

    new-new

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-let loginButton = document.querySelector('.cnopka-login');
-let loginPopUp = document.querySelector('.login-popup');
+const loginButton = document.querySelector('.cnopka-login');
+const loginPopUp = document.querySelector('.login-popup');
+const closeButton = document.querySelector('.cnopka-close');
 
 console.log(loginButton);
 console.log(loginPopUp);
 
 loginButton.addEventListener('click', snowPopUp);
+closeButton.addEventListener('click', close);
 
 function snowPopUp(evt) {
     console.log(evt);
@@ -12,3 +14,11 @@ function snowPopUp(evt) {
 
     loginPopUp.classList.toggle('trigger')
 };
+
+function close() {
+    loginPopUp.classList.remove('trigger')
+};
+
+loginPopUp.addEventListener('click', (e) => {
+    if (e.target.classList.contains(trigger)) {close();}
+})
